e630c661e2e6a9786c34c829e3e59181d138f6e9
quoioln <quoipro94@gmail.com>
2016-12-29 01:14:19 +0700

    move to QLVatTuYeuCau to src v1

--- QLVatTuYeuCau/WebContent/js/date-util.js
+++
@@ -1,5 +0,0 @@
-function parseDate(date) {
-	var temp = date.split("-");
-	var dateResult = temp[2] + '/' + temp[1] + '/' + temp[0];
-	return dateResult;
-}
--- QLVatTuYeuCau/WebContent/js/location.js
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-function getRoot() {
-	var path = location.pathname;
-	var index = path.indexOf("/", 1);
-	var root = path.substring(0, index);
-	return root;
-}
